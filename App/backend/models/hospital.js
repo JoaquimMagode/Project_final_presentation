dad5067a5c9701107e0b7fecd441d68f3ea30a21
naingayewin21-del <naingayewin21@gmail.com>
2026-03-27 20:35:45 +0530

    update

--- App/backend/models/hospital.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const HospitalSchema = new mongoose.Schema({
-  name: String,
-  location: String,
-  specialization: String
-});
-
-module.exports = mongoose.model("Hospital", HospitalSchema);
